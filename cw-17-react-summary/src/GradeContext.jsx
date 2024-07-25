7f417d899825ea1cfd154a321bd9ccbac0ccabfe
erfan1q <erfaniakh143@gmail.com>
2024-07-25 17:18:54 +0430

    logic: reducer added

--- cw-17-react-summary/src/GradeContext.jsx
+++ cw-17-react-summary/src/GradeContext.jsx
@@ -1,22 +1,34 @@
 import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
+const reducer = (state, action) => {
+    switch (action.type){
+        case "SET_GRADE" :
+            return {...state, gradeList:action.data}
+            break;
+            case "CALCULATE" :
+                return 
+          default :
+                return state
+    }
+};
 
 export const gradeContext = createContext();
-export const gradeProvider = ({ children }) => {
+export const GradeProvider = ({ children }) => {
   const [grades, setGrades] = useState([]);
-
+  const [gradeInfo, dispatch] = useReducer(reducer, { gradeList: [], averageScroe: null });
   useEffect(() => {
     const getGrades = async () => {
       try {
         const res = await axios.get("http://localhost:3000/grades");
         setGrades(res.data);
+        dispatch({type:"SET_GRADE", data:res.data})
         console.log(res.data);
       } catch (e) {
         console.log(e.message);
       }
-      getGrades();
     };
-}, []);
+    getGrades();
+  }, []);
   return (
     <gradeContext.Provider value={{ grades }}>{children}</gradeContext.Provider>
   );
--- cw-17-react-summary/src/main.jsx
+++ cw-17-react-summary/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { GradeProvider } from "./GradeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <gradeProvider>
+    <GradeProvider>
       <App />
-    </gradeProvider>
+    </GradeProvider>
+    
   </React.StrictMode>
 );
